5b2ec6c66cb20f658758b2460b4f64fcc91d96b8
Heba Elsayed <hebaabobaa00@gmail.com>
2024-09-03 20:43:46 +0300

    .

--- Domain/Models/Document.cs
+++ Domain/Models/Document.cs
@@ -12,13 +12,7 @@ namespace Domain.Models
     {
         [Key]
         public int Id { get; set; }
-
-        //[Required]
-        //[MaxLength(255)]
         public string Name { get; set; }
-
-        //[Required]
-        //[MaxLength(255)]
 
         [MaxLength(50)]
         public string Type { get; set; }
